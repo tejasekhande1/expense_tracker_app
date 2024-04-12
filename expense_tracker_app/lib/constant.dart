027c2a03ea05bb28c65a3d55ee6f0bf801dc026c
tejasnoovosoft <tejas.noovosoft@gmail.com>
2024-04-12 17:09:06 +0530

    change constant variable names

--- expense_tracker_app/lib/constant.dart
+++ expense_tracker_app/lib/constant.dart
@@ -4,8 +4,8 @@ class AppConstant {
   static const String appName = "Montra";
 
   // colors
-  static const Color primaryButtonColor = Color(0xFF7F3DFF);
-  static const Color secondaryButtonColor = Color(0xFFEEE5FF);
+  static const Color primaryBackgroundColor = Color(0xFF7F3DFF);
+  static const Color secondaryBackgroundColor = Color(0xFFEEE5FF);
   static const Color primaryTextColor = Color(0xFF212325);
   static const Color subTitleColor = Color(0xFF91919F);
   static const Color expenseColor = Color(0xFFFD3C4A);
