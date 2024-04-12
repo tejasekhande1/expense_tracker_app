b17fb217ba7e524ca433aac53bcd2d7f1b054450
tejasnoovosoft <tejas.noovosoft@gmail.com>
2024-04-12 17:04:28 +0530

    add constant.dart

---
+++ expense_tracker_app/lib/constant.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static const String appName = "Montra";
+
+  // colors
+  static const Color primaryButtonColor = Color(0xFF7F3DFF);
+  static const Color secondaryButtonColor = Color(0xFFEEE5FF);
+  static const Color primaryTextColor = Color(0xFF212325);
+  static const Color subTitleColor = Color(0xFF91919F);
+  static const Color expenseColor = Color(0xFFFD3C4A);
+  static const Color incomeColor = Color(0xFF00A86B);
+  static const Color transferColor = Color(0xFF0077FF);
+}
--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_app/constant.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(AppConstant.appName),
         ),
       ),
     );
